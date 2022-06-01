4ece52d08552d9eca9ab241058c1e0a0d0e62880
svetlanach8 <106346758+svetlanach8@users.noreply.github.com>
2022-06-01 23:33:55 +0300

    Update script.js
    correction_tabs

--- Function/script.js
+++ Function/script.js
@@ -11,37 +11,37 @@ let service2 = prompt("Какой дополнительный тип услуг
 let servicePrice2 = prompt("Сколько это будет стоить?");
 
 let getAllServicePrices = function (price1, price2) {
-	return parseFloat(price1) + parseFloat(price2);
+  return parseFloat(price1) + parseFloat(price2);
 }
 
 function getFullPrice(priceLayout, priceAdditional) {
-	return parseFloat(priceLayout) + parseFloat(priceAdditional);
+  return parseFloat(priceLayout) + parseFloat(priceAdditional);
 }
 
 let getTitle = function (text) {
-	let textCorr = text.trimStart();
-	let registTitle = textCorr[0].toUpperCase() + textCorr.substring(1).toLowerCase();
-	return registTitle;
+  let textCorr = text.trimStart();
+  let registTitle = textCorr[0].toUpperCase() + textCorr.substring(1).toLowerCase();
+  return registTitle;
 }
 
 let getServicePercentPrices = function (priceTotal, priceRollaback) {
-	return priceTotal - priceRollaback;
+  return priceTotal - priceRollaback;
 }
 
 let showTypeOf = function (variable) {
-	console.log(variable, typeof variable);
+  console.log(variable, typeof variable);
 }
 
 let getRollbackMessage = function (price) {
-	if (price >= 30000) {
-		return "Даем скидку в 10%";
-	} else if (price > 15000 && price < 30000) {
-		return "Даем скидку в 5%";
-	} else if (price >= 0 && price <= 15000) {
-		return "Скидка не предусмотрена";
-	} else {
-		return "Что то пошло не так";
-	}
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price > 15000 && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 && price <= 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что то пошло не так";
+  }
 }
 
 let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
@@ -56,4 +56,3 @@ showTypeOf(adaptive);
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
-
